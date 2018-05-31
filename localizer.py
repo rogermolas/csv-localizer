acb9837363a05bd18d5dcf19b57729d51949a88a
Roger Molas <r.molas06@gmail.com>
2018-05-31 23:53:11 +0800

    Add android localizer

--- localizer.py
+++ localizer.py
@@ -1,8 +1,10 @@
-import datetime
 import os
 import sys
 import csv
 import json
+
+from ios_localizer import start_localize_ios
+from android_localizer import start_localize_android
 
 CURRENT_DIR = os.path.dirname(__file__)
 
@@ -12,6 +14,7 @@ def main():
   with open(os.path.join(CURRENT_DIR, "config.json")) as setting_file:
     setting = json.load(setting_file)
 
+    PLATFORM = setting["PLATFORM"]
     IN_PATH = setting["IN_PATH"]
     OUT_PATH = setting["OUT_PATH"]
     LANG_KEYS = setting["LANG_KEYS"]
@@ -26,61 +29,13 @@ def main():
     print "Lang keys: {0}".format(LANG_KEYS)
     print "------------------------------------\nSTART..."
 
-    start_localize(BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
+    if PLATFORM == "ios":
+      start_localize_ios(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
+    else:
+      start_localize_android(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
     
     print 'DONE LOCALIZING.'
     print '\n\n'
-
-
-def start_localize(BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
-
-  base_out_dir = os.path.join(BASE_PATH, OUT_PATH)
-  # top most
-  if not os.path.exists(base_out_dir):
-      os.makedirs(base_out_dir)
-
-  # each languages
-  for lang in LANG_KEYS:
-    lang_path = os.path.join(base_out_dir, "{0}.lproj/".format(lang))
-    if not os.path.exists(lang_path):
-      os.makedirs(lang_path)
-
-
-  full_out_paths = [os.path.join(base_out_dir, "{0}.lproj/".format(langKey) + "Localizable.strings") for langKey in LANG_KEYS]
-  allwrites = [open(out_path, 'w') for out_path in full_out_paths]
-
-  for dirname, dirnames, filenames in os.walk(os.path.join(CURRENT_DIR, IN_PATH)):
-    for f in filenames:
-      filename, ext = os.path.splitext(f)
-      if ext != '.csv':
-        continue
-
-      fullpath = os.path.join(dirname, f)
-      print 'Localizing: ' + filename + ' ...'
-
-      with open(fullpath, 'rb') as csvfile:
-        [fwrite.write('\n\n\n/*  {0}  */\n\n'.format(filename)) for fwrite in allwrites]
-
-        reader = csv.reader(csvfile, delimiter=',')
-
-        iterrows = iter(reader);
-        next(iterrows) # skip first line (it is header).
-
-        for row in iterrows:
-          row_key = row[0].replace(" ", "")
-          # comment
-          if row_key[:2] == '//':
-            continue
-
-          row_values = [row[i+1] for i in range(len(LANG_KEYS))]
-
-          # if any row is empty, skip it!
-          if any([value == "" for value in row_values]):
-            [fwrite.write('\n') for idx, fwrite in enumerate(allwrites)]
-          else:
-            [fwrite.write('"{key}" = "{lang}";\n'.format(key=row_key, lang=row_values[idx])) for idx, fwrite in enumerate(allwrites)]
-  [fwrite.close() for fwrite in allwrites]
-
 
 if __name__ == '__main__':
     main()
