2bca193558005fca4caa022a92256d093e0f5d5b
Roger Molas <r.molas06@gmail.com>
2018-07-05 13:54:20 +0800

    add dynamic input output path

--- csv-localizer.py
+++ csv-localizer.py
@@ -1,70 +1,65 @@
 import sys, argparse, logging, os, csv
 from android_localizer import start_localize_android
 
-CURRENT_DIR = os.path.dirname(__file__)
-
 # Gather our code in a main() function
 def main(args, loglevel):
-  logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-  if args.platform == 'ios':
-    logging.debug("ios")
-  elif args.platform == "android":
-    logging.debug("android")
-  else:
-    logging.warn("Invalid platform, platform should be ios or android only")
-
-  logging.debug("Your Argument: %s" % args.platform)
-  logging.debug("Your Argument: %s" % args.input)
-  logging.debug("Your Argument: %s" % args.output)
-  
-  logging.info("Start Localizing .... ")
-
+  # logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+  logging.basicConfig(format="%(message)s", level=loglevel)
   PLATFORM = args.platform
   IN_PATH = args.input
   OUT_PATH = args.output
   LANG_KEYS = ["en", "zh", "ja"] #static will change later
-  BASE_STRINGS_PATH = "currentdir"
-
-  # check input path
+  print '\n'
+  logging.info("Start Localizing .... ")
+  print '\n'
+  logging.info("------------------------------------")
+  
+  # check source path
+  logging.debug("\n")
+  logging.debug("Validating source path ...")
+  logging.debug("\n")
   if not os.path.exists(IN_PATH):
     logging.error('Source path not found, Invalid path.')
+    logging.debug("\n")
     return
   
+  logging.debug("Valid source path, finding csv file ...")
+  logging.debug("\n")
+  logging.debug("Validating target path ...")
+  logging.debug("\n")
   # check output path
   if not os.path.exists(OUT_PATH):
     logging.error('Target path not found, Invalid path.')
+    logging.debug("\n")
     return
+  logging.debug("Valid target path, generating output directory ...")
+  logging.debug("\n")
   
   # generate output directory
   OUTPUT_DIR = os.path.join(OUT_PATH, "output")
-  os.makedirs(OUTPUT_DIR)
-
-  
-  if BASE_STRINGS_PATH == "currentdir":
-    BASE_STRINGS_PATH = CURRENT_DIR
-
-  logging.info("------------------------------------\nSTART...")
+  if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+    logging.debug("Output directory generated : %s" % OUTPUT_DIR)
+    logging.debug("\n")
+  else:
+    logging.debug("Using output directory: %s" % OUTPUT_DIR)
+    logging.debug("\n")
 
   if PLATFORM == "ios":
-    start_localize_ios(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUTPUT_DIR, LANG_KEYS)
-  elif PLATFORM == "android":
-    start_localize_android(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
+    logging.debug("Platform : %s" % PLATFORM)
+    logging.debug("\n")
+    logging.info("Generation output : %s" % OUTPUT_DIR)
+    start_localize_ios(IN_PATH, OUTPUT_DIR, LANG_KEYS)
   else:
+    logging.warn("Invalid platform, platform should be ios or android only")
+    logging.debug("\n")
     logging.error('ERROR LOCALIZING.')
-    logging.error('Unsupported format \n\n')
-    pass
+  print '\n'
+  logging.info("DONE LOCALIZING.")
+  print '\n'
 
-    print 'DONE LOCALIZING.'
-    print '\n'
-
-
-def start_localize_ios(CURRENT_DIR, BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
-
-  base_out_dir = OUT_PATH
-  # top most
-  if not os.path.exists(base_out_dir):
-      os.makedirs(base_out_dir)
-
+def start_localize_ios(SOURCE_PATH, OUTPUT_PATH, LANG_KEYS):
+  base_out_dir = OUTPUT_PATH
   # each languages
   for lang in LANG_KEYS:
     lang_path = os.path.join(base_out_dir, "{0}.lproj/".format(lang))
@@ -74,20 +69,19 @@ def start_localize_ios(CURRENT_DIR, BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
   full_out_paths = [os.path.join(base_out_dir, "{0}.lproj/".format(langKey) + "Localizable.strings") for langKey in LANG_KEYS]
   allwrites = [open(out_path, 'w') for out_path in full_out_paths]
 
-  for dirname, dirnames, filenames in os.walk(os.path.join(CURRENT_DIR, IN_PATH)):
+  for dirname, dirnames, filenames in os.walk(SOURCE_PATH):
     for f in filenames:
       filename, ext = os.path.splitext(f)
       if ext != '.csv':
         continue
 
       fullpath = os.path.join(dirname, f)
-      print 'Localizing: ' + filename + ' ...'
+      logging.info("Localizing: %s", filename)
 
       with open(fullpath, 'rb') as csvfile:
         [fwrite.write('\n\n\n/*  {0}  */\n\n'.format(filename)) for fwrite in allwrites]
 
         reader = csv.reader(csvfile, delimiter=',')
-
         iterrows = iter(reader);
         next(iterrows) # skip first line (it is header).
 
@@ -98,7 +92,6 @@ def start_localize_ios(CURRENT_DIR, BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
             continue
 
           row_values = [row[i+1] for i in range(len(LANG_KEYS))]
-
           # if any row is empty, skip it!
           if any([value == "" for value in row_values]):
             [fwrite.write('\n') for idx, fwrite in enumerate(allwrites)]
@@ -106,14 +99,13 @@ def start_localize_ios(CURRENT_DIR, BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
             [fwrite.write('"{key}" = "{lang}";\n'.format(key=row_key, lang=row_values[idx])) for idx, fwrite in enumerate(allwrites)]
   [fwrite.close() for fwrite in allwrites]
   
-# Load Settings File
+
 # Standard boilerplate to call the main() function to begin
-# the program.
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description = "Locatization commands")
   parser.add_argument("-p",help="Specify Platform (iOS, Android)" ,dest="platform", type=str, required=True)
-  parser.add_argument("-i",help="Source path for CSV file" ,dest="input", type=str, required=True)
-  parser.add_argument("-o",help="Output for localizable files" ,dest="output", type=str, required=True)
+  parser.add_argument("-i",help="Input source, CSV file path" ,dest="input", type=str, required=True)
+  parser.add_argument("-o",help="Generated output path for localizable files" ,dest="output", type=str, required=True)
 
   parser.add_argument("-v",
                       "--verbose",
@@ -126,5 +118,4 @@ if __name__ == '__main__':
     loglevel = logging.DEBUG
   else:
     loglevel = logging.INFO
-
   main(args, loglevel)
