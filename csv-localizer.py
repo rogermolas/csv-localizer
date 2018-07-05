49a2dac3054bce4528de2cf257e7c40229d606b7
Roger Molas <r.molas06@gmail.com>
2018-07-05 12:49:32 +0800

    Add output dir from param

--- csv-localizer.py
+++ csv-localizer.py
@@ -1,6 +1,4 @@
-import sys, argparse, logging, os
-
-from ios_localizer import start_localize_ios
+import sys, argparse, logging, os, csv
 from android_localizer import start_localize_android
 
 CURRENT_DIR = os.path.dirname(__file__)
@@ -26,6 +24,21 @@ def main(args, loglevel):
   OUT_PATH = args.output
   LANG_KEYS = ["en", "zh", "ja"] #static will change later
   BASE_STRINGS_PATH = "currentdir"
+
+  # check input path
+  if not os.path.exists(IN_PATH):
+    logging.error('Source path not found, Invalid path.')
+    return
+  
+  # check output path
+  if not os.path.exists(OUT_PATH):
+    logging.error('Target path not found, Invalid path.')
+    return
+  
+  # generate output directory
+  OUTPUT_DIR = os.path.join(OUT_PATH, "output")
+  os.makedirs(OUTPUT_DIR)
+
   
   if BASE_STRINGS_PATH == "currentdir":
     BASE_STRINGS_PATH = CURRENT_DIR
@@ -33,7 +46,7 @@ def main(args, loglevel):
   logging.info("------------------------------------\nSTART...")
 
   if PLATFORM == "ios":
-    start_localize_ios(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
+    start_localize_ios(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUTPUT_DIR, LANG_KEYS)
   elif PLATFORM == "android":
     start_localize_android(CURRENT_DIR, BASE_STRINGS_PATH, IN_PATH, OUT_PATH, LANG_KEYS)
   else:
@@ -43,8 +56,57 @@ def main(args, loglevel):
 
     print 'DONE LOCALIZING.'
     print '\n'
+
+
+def start_localize_ios(CURRENT_DIR, BASE_PATH, IN_PATH, OUT_PATH, LANG_KEYS):
+
+  base_out_dir = OUT_PATH
+  # top most
+  if not os.path.exists(base_out_dir):
+      os.makedirs(base_out_dir)
+
+  # each languages
+  for lang in LANG_KEYS:
+    lang_path = os.path.join(base_out_dir, "{0}.lproj/".format(lang))
+    if not os.path.exists(lang_path):
+      os.makedirs(lang_path)
+
+  full_out_paths = [os.path.join(base_out_dir, "{0}.lproj/".format(langKey) + "Localizable.strings") for langKey in LANG_KEYS]
+  allwrites = [open(out_path, 'w') for out_path in full_out_paths]
+
+  for dirname, dirnames, filenames in os.walk(os.path.join(CURRENT_DIR, IN_PATH)):
+    for f in filenames:
+      filename, ext = os.path.splitext(f)
+      if ext != '.csv':
+        continue
+
+      fullpath = os.path.join(dirname, f)
+      print 'Localizing: ' + filename + ' ...'
+
+      with open(fullpath, 'rb') as csvfile:
+        [fwrite.write('\n\n\n/*  {0}  */\n\n'.format(filename)) for fwrite in allwrites]
+
+        reader = csv.reader(csvfile, delimiter=',')
+
+        iterrows = iter(reader);
+        next(iterrows) # skip first line (it is header).
+
+        for row in iterrows:
+          row_key = row[0].replace(" ", "")
+          # comment
+          if row_key[:2] == '//':
+            continue
+
+          row_values = [row[i+1] for i in range(len(LANG_KEYS))]
+
+          # if any row is empty, skip it!
+          if any([value == "" for value in row_values]):
+            [fwrite.write('\n') for idx, fwrite in enumerate(allwrites)]
+          else:
+            [fwrite.write('"{key}" = "{lang}";\n'.format(key=row_key, lang=row_values[idx])) for idx, fwrite in enumerate(allwrites)]
+  [fwrite.close() for fwrite in allwrites]
   
-  # Load Settings File
+# Load Settings File
 # Standard boilerplate to call the main() function to begin
 # the program.
 if __name__ == '__main__':
